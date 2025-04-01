35d0030f899db84995b90b6cf6f3451965624249
thiruniimasha <t.hiruniimasha2002@gmail.com>
2025-04-01 13:27:31 +0530

    Update script.js
    add js to contact form

--- SE-2021-053/script.js
+++ SE-2021-053/script.js
@@ -11,3 +11,29 @@ function opentab(tabname) {
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sidemenu = document.getElementById("sidemenu");
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw48HtB4twhHFgt9AJZOU1L-T0bf6q3Wjz9iwhJ55O7EO8Gq3PYfOPzG_G2OlwBYOnJ/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg = document.getElementById("msg")
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML = "Message Sent Successfully"
+            setTimeout(function(){
+                msg.innerHTML = ""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
