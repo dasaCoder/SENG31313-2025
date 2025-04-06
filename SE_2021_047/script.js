2d0922e286c33431807882d123890ad982d1dbc4
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-06 19:55:44 +0530

    render real data to popup

--- SE_2021_047/script.js
+++ SE_2021_047/script.js
@@ -20,3 +20,33 @@ tabsContainer.addEventListener('click', (e) => {
   }
 
 });
+
+// portfolio popup scripts
+
+document.addEventListener('click', (e) => {
+  if(e.target.classList.contains('view-project-btn')){
+    togglePortfolioPopup();
+    portfolioItemDetails(e.target.parentElement);
+  }
+})
+
+
+function togglePortfolioPopup(){
+  document.querySelector('.portfolio-popup').classList.toggle('open');
+  document.body.classList.toggle('hide-scrolling');
+  document.querySelector('.main').classList.toggle('fade-out');
+}
+
+function portfolioItemDetails(portfolioItem){
+
+    document.querySelector('.pp-thumbnail img').src = 
+    portfolioItem.querySelector('.portfolio-item-thumbnail img').src;
+
+    document.querySelector('.pp-header h3').innerHTML =
+    portfolioItem.querySelector('.portfolio-item-title').innerHTML;
+  
+    document.querySelector('.pp-body').innerHTML =
+    portfolioItem.querySelector('.portfolio-item-details').innerHTML;
+}
+
+document.querySelector('.pp-close').addEventListener('click', togglePortfolioPopup);
