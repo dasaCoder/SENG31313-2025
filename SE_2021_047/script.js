e7511cf9b47e0acc7f954734e255e62eefd0443e
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-08 00:05:35 +0530

    link each sections to each buttons

--- SE_2021_047/script.js
+++ SE_2021_047/script.js
@@ -19,8 +19,22 @@ function toggleNavbar(){
 
 document.addEventListener("click",(e)=>{
   if(e.target.classList.contains("link-item")&& e.target.hash !==""){
-    const hash =e.target.hash;
-    console.log(hash);
+      navToggler.classList.add("hide")
+    if(e.target.classList.contains("nav-item")){
+          toggleNavbar();
+    }
+    else{
+          hideSection();
+          document.body.classList.add("hide-scrolling")
+    }
+    setTimeout(()=>{
+      document.querySelector("section.active").classList.remove("active","fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0,0);
+      document.body.classList.remove("hide-scrolling");
+      navToggler.classList.remove("hide")
+
+    },500);
   }
 })
 
