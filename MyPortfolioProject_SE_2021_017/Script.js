a695af3e35079ea7eb3012c16fb0b11eefef9bb1
rashmika <rashmikanethsarani119@gmail.com>
2025-04-09 15:15:58 +0530

    completed the basic css and html code and some js parts to make this more interactive.not responsive yet

--- MyPortfolioProject_SE_2021_017/Script.js
+++ MyPortfolioProject_SE_2021_017/Script.js
@@ -3,6 +3,11 @@ const video2=document.getElementById('projectVideo2');
 const video3=document.getElementById('projectVideo3');
 const hoverSign=document.querySelector('.hover-sign');
 
+//sidebar elements//
+const sidebar=document.querySelector('.sidebar');
+const menu=document.querySelector('.menu-icon');
+const close=document.querySelector('.close-icon');
+
 const videoList=[video1,video2,video3];
 
 videoList.forEach(function(video){
@@ -17,4 +22,16 @@ videoList.forEach(function(video){
         video.pause();
         hoverSign.classList.remove('hover-sign-active');
     })
+})
+
+//sidebar elements//
+menu.addEventListener('click',function(){
+    sidebar.classList.remove('close-sidebar');
+    sidebar.classList.add('open-sidebar');
+})
+
+close.addEventListener('click',function(){
+    sidebar.classList.remove('open-sidebar');
+    sidebar.classList.add('close-sidebar');
+    
 })
