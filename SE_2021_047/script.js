fe68b33e07c98da9ae169714b2be2dba89d8b541
Nadun Dananjaya <146801994+wgnadun@users.noreply.github.com>
2025-04-09 19:37:49 +0530

    change loading time

--- SE_2021_047/script.js
+++ SE_2021_047/script.js
@@ -8,7 +8,7 @@ window.addEventListener("load",()=>{
   document.querySelector(".page-loader").classList.add("fade-out");
   setTimeout(()=>{
     document.querySelector(".page-loader").style.display = "none";
-  },2000);
+  },1000);
 });
 
 
