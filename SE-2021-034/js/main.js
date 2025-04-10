ec0d79ba7f2131cbbcc1a260d56f8c0076f1a34a
danindu2024 <daninduransika@gmail.com>
2025-04-11 00:06:04 +0530

    Added projects section

--- SE-2021-034/js/main.js
+++ SE-2021-034/js/main.js
@@ -2,3 +2,26 @@ window.addEventListener("scroll", function() {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// projecr section - modal
+const projectModals = document.querySelectorAll(".projects-model");
+const imgCards = document.querySelectorAll(".img-card");
+const projectCloseBtns = document.querySelectorAll(".projects-close-btn");
+
+var projectModal = function(modalClicks) {
+    projectModals[modalClicks].classList.add("active");
+}
+
+imgCards.forEach((imgCard, i) => {
+    imgCard.addEventListener("click", () => {
+        projectModal(i);
+    });
+});
+
+projectCloseBtns.forEach((projectCloseBtn) => {
+    projectCloseBtn.addEventListener("click", () => {
+        projectModals.forEach((projectModalView) => {
+            projectModalView.classList.remove("active");
+        });
+    });
+});
